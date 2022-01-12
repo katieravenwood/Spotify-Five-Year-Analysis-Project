6cabf2c048406714ebc8230a2fd983e8b1faa710
Katie Ravenwood <17629242+katieravenwood@users.noreply.github.com>
2022-01-12 11:19:42 -0500

    Initial upload Katie R Sandbox

---
+++ rscripts/KatieRSandbox.R
@@ -0,0 +1,7 @@
+# Katie R Sandbox
+
+## This file is not intended to be run top to bottom!
+
+#### ======================================================
+# K-means testing for exploratory analysis
+
